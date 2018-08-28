2add3a0b0d51c729123fde1957bdef9bdda47449
shanghaitao <359310444@qq.com>
2018-08-28 09:51:46 +0800

    debug is default

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { dbConf } from 'config/db';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { UserModule } from 'routers/user/module';
 
 @Module({
     imports: [TypeOrmModule.forRoot(dbConf), UserModule],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
