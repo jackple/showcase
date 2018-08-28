083731fadeb9083b9b059d7a556e45916c6b74b4
shanghaitao <359310444@qq.com>
2018-08-28 12:32:34 +0800

    up

--- src/routers/user/interface.ts
+++ src/routers/user/interface.ts
@@ -3,6 +3,7 @@ import { Document } from 'mongoose';
 export type UserCategory = 'user' | 'admin';
 
 export interface IUser extends Document {
+    updatedAt: Date;
     createdAt: Date;
     account: string;
     password: string;
