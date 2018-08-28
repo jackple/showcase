ae368153a408a54d6e68273ceab9b88a82fa2bbb
shanghaitao <359310444@qq.com>
2018-08-28 11:28:07 +0800

    up MongooseModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { MongooseModule } from '@nestjs/mongoose';
 
-import { dbConf } from 'config/db';
+import { DB_CONN } from 'config/db';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import modules from 'routers';
 
 @Module({
-    imports: [TypeOrmModule.forRoot(dbConf), ...modules],
+    imports: [
+        MongooseModule.forRoot(DB_CONN, {
+            useNewUrlParser: true,
+        }),
+        ...modules,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
--- src/config/db.ts
+++ src/config/db.ts
@@ -1,15 +1,5 @@
-import * as path from 'path';
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_PORT = process.env.DB_PORT || 27017;
+const DB_NAME = process.env.DB_NAME || 'test';
 
-const getEntityFiles = (fileName: string) => {
-    return path.join(__dirname + `./../**/${fileName}`);
-};
-
-export const dbConf: TypeOrmModuleOptions = {
-    type: 'mongodb',
-    host: process.env.DB_HOST || 'localhost',
-    port: Number(process.env.DB_PORT) || 27017,
-    database: process.env.DB_NAME || 'test',
-    entities: [getEntityFiles('entity.ts'), getEntityFiles('**.entity.ts')],
-    synchronize: true,
-};
+export const DB_CONN = `mongodb://${DB_HOST}:${DB_PORT}/DB_NAME`;
