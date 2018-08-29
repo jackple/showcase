1847dc50930431d81fbe98beaffe7ec3258b68c3
shanghaitao <359310444@qq.com>
2018-08-29 09:12:13 +0800

    config JWT_SECRET JWT_CERT

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,12 @@
 import * as path from 'path';
 import { extractKey } from 'utils/common';
 
+// jwt私钥
 export const JWT_SECRET = extractKey(
-    path.join(__dirname, './../assets/jwt/jwtRS256.key'),
+    path.join(__dirname, '../assets/jwt/jwtRS256.key'),
+);
+
+// jwt公钥
+export const JWT_CERT = extractKey(
+    path.join(__dirname, '../assets/jwt/jwtRS256.key.pub'),
 );
