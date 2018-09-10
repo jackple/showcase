9a609a966a94ea6872dc3c6915adbc73ba832409
shanghaitao <359310444@qq.com>
2018-09-10 17:49:09 +0800

    add pass to mongodb

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,5 +1,7 @@
 const DB_HOST = process.env.DB_HOST || 'localhost'
 const DB_PORT = process.env.DB_PORT || 27017
 const DB_NAME = process.env.DB_NAME || 'test'
+const DB_USER = process.env.DB_USER || 'username'
+const DB_PASS = process.env.DB_PASS || 'password'
 
-export const DB_CONN = `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`
+export const DB_CONN = `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
